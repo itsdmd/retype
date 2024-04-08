83256f6b0aab1ed7e9244d929ea06ea81e494876
Đức Đào <duc.dao.431@gmail.com>
2024-04-08 16:30:43 +0700

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import vitePluginRequire from "vite-plugin-require";
 
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.VITE_PORT || 3000,
-    host: process.env.VITE_HOST || "localhost",
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: { "process.env": env },
+    plugins: [react(), vitePluginRequire.default()],
+    server: {
+      port: env.VITE_PORT || 3000,
+      host: env.VITE_HOST || "localhost",
+    },
+  };
 });
