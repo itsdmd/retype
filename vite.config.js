4c694e973019a187fdaab0a3647cf556b35b3569
Đức Đào <duc.dao.431@gmail.com>
2024-04-08 11:51:06 +0700

    env config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.VITE_PORT || 3000,
+    host: process.env.VITE_HOST || "localhost",
+  },
+});
